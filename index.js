e0aedcb6a106bc499bc5dfd16ee1630a5287d656
qaisaru@icloud.com <qaisaru@icloud.com>
2018-07-08 07:11:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  const spy = string.toUpperCase()
+  console.log(spy)
+}
+
+function logWhisper(string) {
+  const spy = string.toLowerCase()
+  console.log(spy)
+}
+
+function sayHiToGrandma(string) {
+string.toLowerCase() === string
+  return "I can\'t hear you!" }
+  
+function sayHiToGrandma(string) {
+string.toUpperCase() === string
+  return "YES INDEED!" }
+  
+function sayHiToGrandma(string) {
+string.toLowerCase() === string
+  return "I can\'t hear you!" }
+
+
+ 
