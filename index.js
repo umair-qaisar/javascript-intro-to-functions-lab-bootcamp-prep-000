bd735db9fc0bbe2eb7c37f5eac5eba4825533909
qaisaru@icloud.com <qaisaru@icloud.com>
2018-07-08 07:14:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,19 +19,11 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
 if (string.toLowerCase() === string){
   return "I can\'t hear you!" } else if (string.toUpperCase() === string){return "YES INDEED!"}
-  
+}
 
 function sayHiGhToGrandma(string) {
 string.toLowerCase() === string
   return "I can\'t hear you!" }
 
-
-if (dog === "hungry") {
-  console.log("Refilling food bowl.")
-} else if (dog === "thirsty") {
-  console.log("Refilling water bowl.")
-} else {
-  console.log("Reading newspaper.")
-}
 
  
